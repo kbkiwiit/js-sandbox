374a944bc1218538104de7a6e2c0db7da70ab603
Kurt <kelliano@gmail.com>
2019-07-09 15:31:56 +1200

    updates to file

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+/**
+ * PROTOTYPES EXPLAINED
+ */
+
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = dob;
+    this.calculateAge = function() {
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970)
+    }
+}
+
+const john = new Person('John', 'Doe', '08-01-2000');
+const alice = new Person('Alice', 'Allison', '06-01-1991');
+
+console.log(john);
 
 /**
  * OBJECT ORIENTED JS
@@ -35,31 +54,31 @@
 
 /* SUB CLASSES AND INHERITANCE */
 
-class Person {
-    constructor(firstName, lastName)   {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+// class Person {
+//     constructor(firstName, lastName)   {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
 
-    greeting() {
-        return `Hey there ${this.firstName} ${this.lastName}`;
-    }
+//     greeting() {
+//         return `Hey there ${this.firstName} ${this.lastName}`;
+//     }
 
-    static getMembershipCost() {
-        return 1000;
-    }
-}
+//     static getMembershipCost() {
+//         return 1000;
+//     }
+// }
 
-//this is the subclass
-class Customer extends Person {
-    constructor(firstName, lastName, phone, membership) {
-        super(firstName, lastName);
+// //this is the subclass
+// class Customer extends Person {
+//     constructor(firstName, lastName, phone, membership) {
+//         super(firstName, lastName);
 
-        this.phone = phone;
-        this.membership = membership;
-    }
-}
+//         this.phone = phone;
+//         this.membership = membership;
+//     }
+// }
 
-const Kurt = new Customer('John', 'Wick', '09-300-0000', 'Premier');
+// const Kurt = new Customer('John', 'Wick', '09-300-0000', 'Premier');
 
-console.log(Customer.getMembershipCost());
+// console.log(Customer.getMembershipCost());
